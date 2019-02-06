75bcc0183d931b356fbedbfaff5777f891f38565
wawatok <w.tokarski@gmx.net>
2019-02-06 12:49:02 +0100

    better debug courtesy of kalamatee.

--- AROS/rom/exec/doio.c
+++ AROS/rom/exec/doio.c
@@ -1,7 +1,6 @@
 /*
     Copyright © 1995-2007, The AROS Development Team. All rights reserved.
     $Id$
-
     Desc: Start an IO request and wait until it completes.
     Lang: english
 */
@@ -14,7 +13,6 @@
 #include <aros/debug.h>
 
 /*****************************************************************************
-
     NAME */
 
 	AROS_LH1(LONG, DoIO,
@@ -28,57 +26,49 @@
 /*  FUNCTION
 	Start an I/O request by calling the devices's BeginIO() vector.
 	It waits until the request is complete.
-
     INPUTS
 	iORequest - Pointer to iorequest structure.
-
     RESULT
-
     NOTES
 	OpenDevice() notes explain LONG return type.
-
     EXAMPLE
-
     BUGS
-
     SEE ALSO
 	OpenDevice(), CloseDevice(), DoIO(), SendIO(), AbortIO(), WaitIO()
-
     INTERNALS
-
 ******************************************************************************/
 {
     AROS_LIBFUNC_INIT
-D(bug("DoIO: Begin\n"));
+D(bug("[%s:%u] %s(%04x)\n", iORequest->io_Device->dd_Library.lib_Node.ln_Name, iORequest->io_Unit, __func__, iORequest->io_Command));
     /*
 	Prepare the message. Tell the device that it is OK to wait in the
 	BeginIO() call by setting the quick bit.
     */
     ASSERT_VALID_PTR(iORequest);
     if (!iORequest) return -1;
-D(bug("DoIO: iORequest 0x%p\n", iORequest));
+D(bug("[%s:%u] %s: iORequest @ 0x%p\n", iORequest->io_Device->dd_Library.lib_Node.ln_Name, iORequest->io_Unit, __func__, iORequest));
     iORequest->io_Flags=IOF_QUICK;
     iORequest->io_Message.mn_Node.ln_Type=0;
 
     ASSERT_VALID_PTR(iORequest->io_Device);
     if (!iORequest->io_Device) return -1;
-D(bug("DoIO: iORequest->io_Device 0x%p\n", iORequest->io_Device));
+D(bug("[%s:%u] %s: Calling device ...\n", iORequest->io_Device->dd_Library.lib_Node.ln_Name, iORequest->io_Unit, __func__));
     /* Call BeginIO() vector */
     AROS_LVO_CALL1NR(void,
 	AROS_LCA(struct IORequest *,iORequest,A1),
 	struct Device *,iORequest->io_Device,5,
     );
-D(bug("DoIO: iORequest->io_Flags & IOF_QUICK %d\n", iORequest->io_Flags&IOF_QUICK));
+//D(bug("DoIO: iORequest->io_Flags & IOF_QUICK %d\n", iORequest->io_Flags&IOF_QUICK));
+
     /* If the quick flag is cleared it wasn't done quickly. Wait for completion. */
     if(!(iORequest->io_Flags&IOF_QUICK))
       {
-        D(bug("DoIO: Before WaitIO\n"));
+        D(bug("[%s:%u] %s: Waiting for IO to complete ...\n", iORequest->io_Device->dd_Library.lib_Node.ln_Name, iORequest->io_Unit, __func__));
 	WaitIO(iORequest);
-        D(bug("DoIO: After WaitIO\n"));
+        //D(bug("DoIO: After WaitIO\n"));
       }
-D(bug("DoIO: End iORequest->io_Error %d\n", iORequest->io_Error));
+D(bug("[%s:%u] %s: Command %04x io_Error=%d\n", iORequest->io_Device->dd_Library.lib_Node.ln_Name, iORequest->io_Unit, __func__, iORequest->io_Command, iORequest->io_Error));
     /* All done. Get returncode. */
     return iORequest->io_Error;
     AROS_LIBFUNC_EXIT
 } /* DoIO */
-
