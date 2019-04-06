fbb14861a2cc816fc35905b46974d54bbb3bc71a
Michael Ness <mness1978@gmail.com>
2019-04-06 01:53:45 -0500

    Vampire specific changes to the hidd init routine.

--- AROS/rom/hidds/vampiregfx/startup.c
+++ AROS/rom/hidds/vampiregfx/startup.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2017, The Apollo Team. All rights reserved.
+    Copyright © 2017-2019, The Apollo Team. All rights reserved.
 */
 
 #define DEBUG 1
@@ -17,6 +17,7 @@
 
 #include LC_LIBDEFS_FILE
 
+UBYTE BOARDID = 0;
 BOOL Init_VampireGFXClass(LIBBASETYPEPTR LIBBASE);
 
 #undef SysBase
@@ -24,7 +25,7 @@ BOOL Init_VampireGFXClass(LIBBASETYPEPTR LIBBASE);
 
 static int VampireGFX_Init(LIBBASETYPEPTR LIBBASE)
 {
-    ULONG err;
+    ULONG err = 0;
     struct ExecBase *SysBase = (LIBBASE)->csd.cs_SysBase;
     struct Library  *GfxBase = TaggedOpenLibrary(TAGGEDOPEN_GRAPHICS);
     struct Library  *OOPBase = OpenLibrary("oop.library", 0);
@@ -37,22 +38,51 @@ static int VampireGFX_Init(LIBBASETYPEPTR LIBBASE)
     LIBBASE->csd.basebm = OOP_FindClass(CLID_Hidd_BitMap);
     CloseLibrary(OOPBase);
 
-    if (!Init_VampireGFXClass(LIBBASE)) {
-        CloseLibrary(GfxBase);
-        return FALSE;
-    }
+    if (!Init_VampireGFXClass(LIBBASE))
+        goto _ABORT;
 
     LIBBASE->library.lib_OpenCnt = 1;
+    BOARDID = ( *(volatile UWORD *)VREG_BOARD ) >> 8;
+
+    if (BOARDID == VREG_BOARD_Unknown ||
+        BOARDID >= VREG_BOARD_Future)
+    {
+        // Not a Vampire Board
+        goto _ABORT;
+    }
+
+
+    //////////////////////////////////////////////////////
+    // If the user is holding down SHIFT, then the driver don't load.
+    //////////////////////////////////////////////////////
+
+    if (0 == OpenDevice("input.device", 0, &io, 0))
+    {
+        struct Library *InputBase = (struct Library *)io.io_Device;
+        UWORD qual = PeekQualifier();
+        CloseDevice(&io);
+
+        if (qual & (IEQUALIFIER_LSHIFT | IEQUALIFIER_RSHIFT))
+            goto _ABORT;
+    }
 
     err = AddDisplayDriver(LIBBASE->csd.gfxclass, NULL,
 			   DDRV_KeepBootMode, TRUE,
 			   DDRV_IDMask      , 0xF0000000,
 			   TAG_DONE);
 
-    CloseLibrary(GfxBase);
+    D(bug("[HiddVampireGfx] AddDisplayDriver() result: %u\n", err));
 
-    D(bug("VampireHIDD AddDisplayDriver() result: %u\n", err));
-    return err ? FALSE : TRUE;
+    if (err)
+        goto _ABORT;
+
+    LIBBASE->library.lib_OpenCnt = 1;
+
+    return TRUE;
+
+_ABORT:
+    CloseLibrary(GfxBase);
+    return FALSE;
 }
 
 ADD2INITLIB(VampireGFX_Init, 0)
